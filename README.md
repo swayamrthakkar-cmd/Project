e56d9d1573f220338cb162dc8c7df1d54e535b3c
swayamrthakkar-cmd <swayam.r.thakkar@gmail.com>
2026-03-17 23:53:49 +0000

    Eval

--- README.md
+++
@@ -1,7 +0,0 @@
-1. board representation
-2. board display
-3. player input
-4. win detection
-5. game loop
-6. random AI
-7. minimax AI
--- chessbot.py
+++ chessbot.py
@@ -1,10 +1,17 @@
 import chess
 import chess.engine
 
-engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+# Standard Algebric Notation (SAN)
+# Universal Chess Interface (UCI)
 
-board = chess.Board()
+# Get Stockfish
 
-result = engine.analyse(board, chess.engine.Limit(depth=15))
+with chess.engine.SimpleEngine.popen_uci("STOCKFISH") as engine:
+    board = chess.Board()
+    result = engine.analyse(board, chess.engine.Limit(depth=15))
 
-print(result["score"])
+    result = str(result["score"]).replace('PovScore(', '').replace('(', ',').replace('', '').replace(')', '').replace(' ', '')
+
+    unit, eval, color = result.split(',')
+
+    print(result)
