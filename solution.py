ebe5610b919279f63ff897cda2b9f0ec0111fb36
swayamrthakkar-cmd <swayam.r.thakkar@gmail.com>
2026-03-31 23:00:42 +0000

    Commit

--- solution.py
+++ solution.py
@@ -1,21 +1,16 @@
-import random
+class solution:
+    def Main():
+        count = 0
 
-n = random.randint(0, 10**3)
-n = 416
+        i = 1
+        while i * i <= n:
+            if n % i == 0:
+                if i * (i + 1) <= n:
+                    count += 1
+                
+                j = n // i
+                if j != i and j * (j + 1) <= n:
+                    count += 1
+            i += 1
 
-print(n)
-
-count = 0
-
-i = 1
-while i * i <= n:
-    if n % i == 0:
-        if i * (i + 1) <= n:
-            count += 1
-        
-        j = n // i
-        if j != i and j * (j + 1) <= n:
-            count += 1
-    i += 1
-
-print(count)
+        print(count)
